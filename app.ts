a6ed4cabed25aed5d294a87871a9a6d35b683dec
bruno-henr <brunohenriquex00@gmail.com>
2024-04-29 21:29:47 -0300

    adding process listening

--- app.ts
+++ app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
-import  { ExpressValidator }  from 'express-validator';
+import { ExpressValidator } from 'express-validator';
 import { router } from 'src/routes';
 
 export const app = express();
 app.use(express.json());
-app.use("/v1/api",router)
+app.use("/v1/api", router)
+
+process.on('uncaughtException', err => {
+    console.error('There was an uncaught error', err)
+    process.exit(1) 
+})
