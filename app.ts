a3684561b44b01346561e5227c363adf37ec6fb6
bruno-henr <brunohenriquex00@gmail.com>
2024-05-01 23:10:12 -0300

    Add CORS middleware to allow cross-origin requests
    The change includes:
    - Importing cors package in app.ts
    - Adding cors middleware to the app instance with origin set to '*'
    - Inst

--- app.ts
+++ app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { router } from 'src/routes';
+import cors from 'cors';
 
 export const app = express();
 app.use(express.json());
+app.use(cors({ origin: '*' }));
 app.use("/v1/api", router)
-
 process.on('uncaughtException', err => {
     console.error('There was an uncaught error', err)
     process.exit(1) 
