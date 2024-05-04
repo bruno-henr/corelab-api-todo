68add3d0e2a5f6126e4ea7ab1d242b4380c54295
bruno_henr <brunohenriquex0@gmail.com>
2024-05-04 15:23:04 -0300

    config: "Add Swagger UI for API documentation"
    This commit adds Swagger UI to the existing Express app for API documentation. The `swagger-ui-express` package has been installed and the `swaggerDocument` is now served at

--- app.ts
+++ app.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import { router } from 'src/routes';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 export const app = express();
+
+
 app.use(express.json());
 app.use(cors({ origin: '*' }));
-app.use("/v1/api", router)
+app.use("/v1/api", router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+    options: { explorer: true }
+}));
 process.on('uncaughtException', err => {
     console.error('There was an uncaught error', err)
-    process.exit(1) 
+    process.exit(1)
 })
